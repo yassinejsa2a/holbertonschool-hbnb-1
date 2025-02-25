bef572b29be30fb46e9d961ad50feba79fa203a3
Jonas-Jungling <jonas.jungling@oclock.school>
2025-02-25 11:11:49 +0100

    refactor model Review

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,19 +1,20 @@
 from app.models.base import BaseModel
 from app.models.user import User
+from app.models.place import Place
 
 class Review(BaseModel):
     """
     Represents a review of a place.
     """
-    def __init__(self, text,rating, user_id, place_id):
+    def __init__(self, text, rating, user, place):
         """
         Initialize a new review.
         """
         super().__init__()
         self.text = text
         self.rating = rating
-        self.user_id = user_id
-        self.place_id = place_id
+        self.user = user
+        self.place = place
 
     @property
     def text(self):
@@ -27,6 +28,8 @@ class Review(BaseModel):
         """
         Set the review's text.
         """
+        if not value or len(value) > 500:
+            raise ValueError('Review text must be provided and be less than 500 characters long.')
         self.__text = value
 
     @property
@@ -41,32 +44,38 @@ class Review(BaseModel):
         """
         Set the review's rating.
         """
+        if not (1 <= value <= 5):
+            raise ValueError('Rating must be between 1 and 5.')
         self.__rating = value
 
     @property
-    def user_id(self):
+    def user(self):
         """
-        Get the review's user_id.
+        Get the review's user.
         """
-        return self.__user_id
+        return self.__user
     
-    @user_id.setter
-    def user_id(self, value):
+    @user.setter
+    def user(self, value):
         """
-        Set the review's user_id.
+        Set the review's user.
         """
-        self.__user_id = value
+        if not isinstance(value, User):
+            raise ValueError('user must be an instance of User.')
+        self.__user = value
 
     @property
-    def place_id(self):
+    def place(self):
         """
-        Get the review's place_id.
+        Get the review's place.
         """
-        return self.__place_id
+        return self.__place
     
-    @place_id.setter
-    def place_id(self, value):
+    @place.setter
+    def place(self, value):
         """
-        Set the review's place_id.
+        Set the review's place.
         """
-        self.__place_id = value
+        if not isinstance(value, Place):
+            raise ValueError('place must be an instance of Place.')
+        self.__place = value
