23199c8809a67819d9ed4d160d0427f6aaa07a26
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-01 14:01:58 +0100

    add flask

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,4 +1,5 @@
 from flask_restx import Api
+from flask import Flask
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
